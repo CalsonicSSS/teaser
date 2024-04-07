36c0b44f1ed826796ff6612b3867e8d9e265747c
CalsonicSheng <calson.developer@gmail.com>
2024-04-07 17:52:44 -0400

    production URL

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -1,7 +1,7 @@
 // from home page to send api to process user query in the backend
 
 export async function sendUserQueryApi(userQuery: string): Promise<Response> {
-  const response = await fetch("http://localhost:5001/query", {
+  const response = await fetch("http://chainn-publi-wuqhol69ne6p-2038441586.us-east-1.elb.amazonaws.com/query", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
