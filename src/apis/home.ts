561017c2c6a84add5e82e1841edf4409d71fc5ec
CalsonicSKC <calson.sheng@chainnect.ai>
2024-04-08 12:25:42 -0400

    update server url

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -1,9 +1,7 @@
 // from home page to send api to process user query in the backend
 
 export async function sendUserQueryApi(userQuery: string): Promise<Response> {
-  console.log("http://chainn-publi-wuqhol69ne6p-2038441586.us-east-1.elb.amazonaws.com/query");
-
-  const response = await fetch("http://chainn-publi-wuqhol69ne6p-2038441586.us-east-1.elb.amazonaws.com/query", {
+  const response = await fetch("https://api.chainnect.ai/query", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
