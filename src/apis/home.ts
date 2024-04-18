814de0f40d8fa09255ed6a45cb7bfa9c08c9cf0f
CalsonicSKC <calson.sheng@chainnect.ai>
2024-04-18 16:41:20 -0400

    fix

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -3,7 +3,7 @@
 import { local, prod } from "@/constants/apiDomain";
 
 export async function sendUserQueryApi(userQuery: string): Promise<Response> {
-  const response = await fetch(`${prod}/query`, {
+  const response = await fetch(`${prod}/query/teaser`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { ReactElement } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/theme/typography";
 
-// as tested, this is the root layout, which acts as top level entry point for the app
-// any client-side navigation will be rendered different page-level component inside this layout (layout first runs, then the target page level component runs)
+// as tested, this is the root layout component, which acts as top level entry point for the app
+// any client-side navigation will render different target page-level component under this layout (this layout first runs, then the target page level component runs)
 export default function RootLayout({ children }: { children: ReactElement }): ReactElement {
   console.log("Root layout runs");
 
